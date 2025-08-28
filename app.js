44a8af6cfcdd67ca513247ff79e006f65c7e70ad
Ameer Moavia <ameermoavia3@gmail.com>
2025-08-28 00:24:46 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// practice 1
+
+// var username = prompt("username");
+// var password = prompt("password");
+
+// if(username === "Ameer Moavia" && password === "12345"){
+//     alert("welcome back!");
+// } else{
+//     alert("Access denied");
+// }
+
+// practice 2
+
+var marks = prompt("Type your marks");
+
+if (marks > 100) {
+     alert("Add marks between 1 to 100;");
+} else if(marks >= 90){
+    alert("The grade is a");
+} else if(marks >= 80){
+    alert("The grade is b");
+} else if(marks >= 70){
+    alert("The grade is c");
+} else{
+    alert("Failed");
+}
